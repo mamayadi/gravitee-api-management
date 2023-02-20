d0574360d5c04cfe1325cff482b4ab78a7239df1
Antoine CORDIER <antoine.cordier@graviteesource.com>
2023-02-20 17:06:15 +0000

    feat: build javascript function and get zip

--- gravitee-apim-plugin/gravitee-apim-plugin-resource/gravitee-apim-plugin-resource-azure-function/src/main/java/io/gravitee/plugin/resource/AzureFunctionResource.java
+++ gravitee-apim-plugin/gravitee-apim-plugin-resource/gravitee-apim-plugin-resource-azure-function/src/main/java/io/gravitee/plugin/resource/AzureFunctionResource.java
@@ -15,24 +15,9 @@
  */
 package io.gravitee.plugin.resource;
 
-import io.gravitee.plugin.resource.functions.azure.Function;
 import io.gravitee.plugin.resource.helpers.java.JavaFunctionBuilder;
 import io.gravitee.resource.api.AbstractConfigurableResource;
-import java.io.*;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-import org.apache.maven.shared.invoker.DefaultInvocationRequest;
-import org.apache.maven.shared.invoker.DefaultInvoker;
-import org.apache.maven.shared.invoker.InvocationRequest;
-import org.apache.maven.shared.invoker.InvocationResult;
-import org.apache.maven.shared.invoker.Invoker;
 
 /**
  * @author Yann TAVERNIER (yann.tavernier at graviteesource.com)
@@ -40,47 +25,16 @@ import org.apache.maven.shared.invoker.Invoker;
  */
 public class AzureFunctionResource extends AbstractConfigurableResource<AzureFunctionResourceConfiguration> {
 
-    private static final String ZIP_FILE_NAME = "graviteeio-azure-function.zip";
-    private static final String HOST_FILE = "azure/host.json";
-    private static final String BINDINGS_FILE = "azure/function.json";
-
     @Override
     protected void doStart() throws Exception {
         super.doStart();
-        zipFunction(new Function(configuration().getFunctionCode()));
         // Get java code from configuration
         // Use what's in resource folder to generate a zip fpr the function
-        // Then, push the zip to azure thanks to https://learn.microsoft.com/en-us/azure/azure-functions/deployment-zip-push#rest
+        // Then, push the zip to javascript thanks to https://learn.microsoft.com/en-us/azure/azure-functions/deployment-zip-push#rest
 
         // ⚠️ what if we restart the gateway and function has already been deployed ? Maybe add a "force redeploy" configuration to override the deployed one.
 
         // FIXME: pass the configuration code as a parameter to generate the function needed by the user.
         final Path javaZipPath = new JavaFunctionBuilder().buildFunction();
-    }
-
-    private void zipFunction(Function function) throws IOException {
-        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(ZIP_FILE_NAME))) {
-            zipFunctionCode(function, zos);
-            zipFunctionDescriptor(HOST_FILE, zos);
-            zipFunctionDescriptor(BINDINGS_FILE, zos);
-            zos.closeEntry();
-        }
-    }
-
-    private void zipFunctionCode(Function function, ZipOutputStream zos) throws IOException {
-        ZipEntry functionEntry = new ZipEntry(function.fileName());
-        zos.putNextEntry(functionEntry);
-        ByteArrayInputStream functionContent = new ByteArrayInputStream(function.toString().getBytes());
-        zos.write(functionContent.readAllBytes());
-    }
-
-    private void zipFunctionDescriptor(String fileName, ZipOutputStream zos) throws IOException {
-        ZipEntry hostEntry = new ZipEntry(fileName);
-        zos.putNextEntry(hostEntry);
-        InputStream stream = getClass().getResourceAsStream(fileName);
-        if (stream == null) {
-            throw new IllegalStateException("Resource " + fileName + "not found");
-        }
-        zos.write(stream.readAllBytes());
     }
 }
---
+++ gravitee-apim-plugin/gravitee-apim-plugin-resource/gravitee-apim-plugin-resource-azure-function/src/main/java/io/gravitee/plugin/resource/helpers/javascript/JavascriptFunctionBuilder.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.plugin.resource.helpers.javascript;
+
+import io.gravitee.plugin.resource.AzureFunctionResourceConfiguration;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * @author GraviteeSource Team
+ */
+public class JavascriptFunctionBuilder {
+
+    private static final String HOST_FILE_NAME = "host.json";
+    private static final String FUNCTION_FILE_NAME = "index.js";
+    private static final String BINDINGS_FILE_NAME = "function.json";
+
+    private static final String RESOURCES = "javascript";
+
+    private static final String LEADING_BLOCK = "module.exports = async function (context, req) {";
+    private static final String CLOSING_BLOCK = "}";
+
+    private final String functionFile;
+    private final String bindingsFile;
+    private final String hostFile;
+    private final String functionCode;
+    private final String zipFile;
+
+    public JavascriptFunctionBuilder(AzureFunctionResourceConfiguration configuration) {
+        functionFile = Path.of(configuration.getFunctionName(), FUNCTION_FILE_NAME).toString();
+        bindingsFile = Path.of(configuration.getFunctionName(), BINDINGS_FILE_NAME).toString();
+        functionCode = configuration.getFunctionCode();
+        zipFile = getZipFileName(configuration);
+        hostFile = HOST_FILE_NAME;
+    }
+
+    @Override
+    public String toString() {
+        return LEADING_BLOCK.concat(functionCode).concat(CLOSING_BLOCK);
+    }
+
+    public Path buildFunction() throws IOException {
+        try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(zipFile))) {
+            addEntry(zip, functionFile, getCodeAsStream());
+            addEntry(zip, bindingsFile, getResourceAsStream(BINDINGS_FILE_NAME));
+            addEntry(zip, hostFile, getResourceAsStream(HOST_FILE_NAME));
+            return Path.of(zipFile);
+        }
+    }
+
+    private void addEntry(ZipOutputStream zip, String fileName, InputStream content) throws IOException {
+        ZipEntry entry = new ZipEntry(fileName);
+        zip.putNextEntry(entry);
+        zip.write(content.readAllBytes());
+        zip.closeEntry();
+        content.close();
+    }
+
+    private InputStream getResourceAsStream(String fileName) {
+        String path = Path.of(RESOURCES, fileName).toString();
+        InputStream stream = getClass().getClassLoader().getResourceAsStream(path);
+        if (stream == null) {
+            throw new IllegalStateException("Resource " + path + " not found");
+        }
+        return stream;
+    }
+
+    private InputStream getCodeAsStream() {
+        return new ByteArrayInputStream(toString().getBytes());
+    }
+
+    private static String getZipFileName(AzureFunctionResourceConfiguration configuration) {
+        return String.format("%s-%s.zip", configuration.getFunctionName(), UUID.randomUUID());
+    }
+}
